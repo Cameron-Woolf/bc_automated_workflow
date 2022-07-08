d08846c57bcc80c425971338ba964495848e82f3
cameron.woolf <cameron.woolf@beatconnect.com>
2022-07-08 17:26:53 -0400

    more navigation tests

--- src/test/java/NavigationTests.java
+++ src/test/java/NavigationTests.java
@@ -13,35 +13,94 @@ import java.util.concurrent.TimeUnit;
 public class NavigationTests {
 
     public WindowsDriver driver;
+    private boolean setUpCompleted = false;
 
     // Ran before each Test Method
     @BeforeMethod
-    public void setUp() {
+    private void setUp() {
 
-        setUpHubSession();
+        if (!setUpCompleted) {
+            setUpHubSession();
+        }
     }
-
 
     // Ran after each Test Method
     @AfterMethod
     private void tearDown() throws InterruptedException {
-        driver.closeApp();
-        driver.quit();
-        Thread.sleep(2000);
+//        driver.closeApp();
+//        driver.quit();
+//        Thread.sleep(2000);
+    }
+    @Test
+    public void navigateBetweenStudio() {
+
+        String studio1 = "Automation Studio 1";
+        String studio2 = "Automation Studio 2";
+        String studio3 = "Automation Studio 3";
+
+        driver.findElementByName(studio1).click();
+        driver.findElementByName(studio2).click();
+        driver.findElementByName(studio3).click();
+
     }
 
     @Test
-    public void openSequencer() throws InterruptedException {
+    public void navigateBetweenAllTabs() {
 
-        String project = "Projects";
-        String studio = "Automation Studio";
-        String openSeq = "Open v0.0.0";
+        // Profile Tab names
+        String youTab = "For you";
+        String exploreTab = "Explore";
+        String myProfile = "My profile";
 
-        driver.findElementByName(studio).click();
-        driver.findElementByName(project).click();
-        driver.findElementByName(openSeq).click();
+        // Studio Tab names
+        String homeTab = "Home";
+        String membersTab = "Members";
+        String projectsTab = "Projects";
+        String settingsTab = "Settings";
 
+        // Tray icon names
+        String helpIcon = "?";
 
+        // Studio and profile names
+        String profile = "To get missing image descriptions, open the context menu.";
+        String studio1 = "Automation Studio 1";
+
+        // Navigate between profile tabs when BC first opens
+        // Must do this before navigating to a studio or test will fail
+        driver.findElementByName(homeTab).click();
+        driver.findElementByName(exploreTab).click();
+        driver.findElementByName(projectsTab).click();
+
+        // Navigate to Automation Studio 1
+        driver.findElementByName(studio1).click();
+
+        // Navigate to each tab inside the studio
+        driver.findElementByName(homeTab).click();
+        driver.findElementByName(membersTab).click();
+        driver.findElementByName(projectsTab).click();
+        driver.findElementByName(settingsTab).click();
+
+        // Navigate to try icons
+        driver.findElementByName(helpIcon).click();
+
+    }
+
+    @Test
+    public void navigateToProject() {
+
+        // Element Names
+        String projectsTab = "Projects";
+        String studio1 = "Automation Studio 1";
+        String project1 = "AS1_Project_1";
+
+        // Navigate to the correct studio
+        driver.findElementByName(studio1).click();
+
+        // Navigate to the projects tab
+        driver.findElementByName(projectsTab).click();
+
+        // Find the project
+        driver.findElementByName(project1);
     }
 
     // Creates a root desktop session that has access to every Window the desktop can see
@@ -80,6 +139,7 @@ public class NavigationTests {
 
         //provide implicit wait of 10 seconds
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        setUpCompleted = true;
 
     }
 
